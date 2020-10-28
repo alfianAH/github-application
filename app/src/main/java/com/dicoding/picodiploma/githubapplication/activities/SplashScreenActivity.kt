1e9eba55768acea21caf22bd1d33d9522bdfc026
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-10-28 16:18:17 +0800

    Make splash screen

---
+++ app/src/main/java/com/dicoding/picodiploma/githubapplication/activities/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package com.dicoding.picodiploma.githubapplication.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.dicoding.picodiploma.githubapplication.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            // Move activity after delay
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
