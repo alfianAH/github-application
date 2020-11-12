9fd2d05c7b9263053a8f2262a0bf24916046b23a
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-11-12 20:34:05 +0800

    Change data type to val

--- app/src/main/java/com/dicoding/picodiploma/githubapplication/User.kt
+++ app/src/main/java/com/dicoding/picodiploma/githubapplication/User.kt
@@ -5,12 +5,12 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class User (
-    var photo: Int,
-    var username: String,
-    var name: String,
-    var location: String,
-    var company: String,
-    var repositories: String,
-    var followers: String,
-    var following: String
+    val photo: Int,
+    val username: String,
+    val name: String,
+    val location: String,
+    val company: String,
+    val repositories: String,
+    val followers: String,
+    val following: String
 ): Parcelable
