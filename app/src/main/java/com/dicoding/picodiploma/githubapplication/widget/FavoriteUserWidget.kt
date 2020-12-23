304249abc280ea376a0eab3d9525b827900000fc
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-12-23 17:04:53 +0800

    Add favorite user stack view widget

---
+++ app/src/main/java/com/dicoding/picodiploma/githubapplication/widget/FavoriteUserWidget.kt
@@ -0,0 +1,71 @@
+package com.dicoding.picodiploma.githubapplication.widget
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.widget.RemoteViews
+import android.widget.Toast
+import androidx.core.net.toUri
+import com.dicoding.picodiploma.githubapplication.R
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class FavoriteUserWidget : AppWidgetProvider() {
+
+    companion object{
+        private const val TOAST_ACTION = "com.dicoding.picodiploma.TOAST_ACTION"
+        const val EXTRA_ITEM = "com.dicoding.picodiploma.EXTRA_ITEM"
+        private val TAG = FavoriteUserWidget::class.java.simpleName
+
+        private fun updateAppWidget(
+            context: Context,
+            appWidgetManager: AppWidgetManager,
+            appWidgetId: Int
+        ) {
+            Log.d(TAG, "updateAppWidget")
+            val intent = Intent(context, StackWidgetService::class.java)
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+            intent.data = intent.toUri(Intent.URI_INTENT_SCHEME).toUri()
+
+            // Construct the RemoteViews object
+            val views = RemoteViews(context.packageName, R.layout.favorite_user_widget)
+            views.setRemoteAdapter(R.id.stack_view, intent)
+            views.setEmptyView(R.id.stack_view, R.id.empty_view)
+
+            val toastIntent = Intent(context, FavoriteUserWidget::class.java)
+            toastIntent.action = TOAST_ACTION
+            toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+            intent.data = intent.toUri(Intent.URI_INTENT_SCHEME).toUri()
+            val toastPendingIntent = PendingIntent.getBroadcast(context, 0, toastIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+            views.setPendingIntentTemplate(R.id.stack_view, toastPendingIntent)
+
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+        if(intent.action != null){
+            if(intent.action == TOAST_ACTION){
+                val viewIndex = intent.getIntExtra(EXTRA_ITEM, 0)
+                Toast.makeText(context, "Touched view $viewIndex", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/dicoding/picodiploma/githubapplication/widget/StackRemoteViewsFactory.kt
@@ -0,0 +1,76 @@
+package com.dicoding.picodiploma.githubapplication.widget
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.util.Log
+import android.widget.RemoteViews
+import android.widget.RemoteViewsService
+import androidx.core.os.bundleOf
+import com.bumptech.glide.Glide
+import com.dicoding.picodiploma.githubapplication.R
+import com.dicoding.picodiploma.githubapplication.database.FavoriteUserHelper
+import com.dicoding.picodiploma.githubapplication.helper.MappingHelper
+import java.lang.Exception
+
+internal class StackRemoteViewsFactory(private val context: Context): RemoteViewsService.RemoteViewsFactory {
+
+    private lateinit var favoriteUserHelper: FavoriteUserHelper
+    private val TAG = StackRemoteViewsFactory::class.java.simpleName
+    private val widgetItems = ArrayList<String>()
+
+    override fun onCreate() {
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onDataSetChanged() {
+        favoriteUserHelper = FavoriteUserHelper.getInstance(context)
+        favoriteUserHelper.open()
+
+        val users = MappingHelper.mapCursorToArrayList(
+            favoriteUserHelper.queryAll()
+        )
+
+        if(users.size > 0) {
+            for (user in users) {
+                widgetItems.add(user.photo)
+            }
+        }
+
+        favoriteUserHelper.close()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy")
+    }
+
+    override fun getCount(): Int = widgetItems.size
+
+    override fun getViewAt(position: Int): RemoteViews {
+        val rv = RemoteViews(context.packageName, R.layout.widget_item)
+
+        try{
+            val bitmap: Bitmap = Glide.with(context)
+                .asBitmap()
+                .load(widgetItems[position])
+                .submit(460, 460)
+                .get()
+
+            rv. setImageViewBitmap(R.id.img_view, bitmap)
+        } catch (e: Exception){
+            e.printStackTrace()
+        }
+
+        val extras = bundleOf(FavoriteUserWidget.EXTRA_ITEM to position)
+        val fillInIntent = Intent()
+        fillInIntent.putExtras(extras)
+
+        rv.setOnClickFillInIntent(R.id.img_view, fillInIntent)
+        return rv
+    }
+
+    override fun getLoadingView(): RemoteViews? = null
+    override fun getViewTypeCount(): Int = 1
+    override fun getItemId(i: Int): Long = 0
+    override fun hasStableIds(): Boolean = false
+}
